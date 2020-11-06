eccd4e3255b853bc841415120aedf0d44ba36b68
dotcom7860 <dotcom7860@naver.com>
2020-11-07 08:53:02 +0900

    메인페이지 구현 중

---
+++ front/src/components/views/mainPage/OutWork.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react'
+import { DatePicker, message, Alert, Layout, Menu, Breadcrumb, Button, Row, Col} from 'antd';
+import 'antd/dist/antd.css';
+
+
+function OutWork() {
+    return(
+        <div>
+            연가페이지
+        </div>
+    );
+}
+
+export default OutWork
--- front/src/components/views/mainPage/mainPage.js
+++ front/src/components/views/mainPage/mainPage.js
@@ -1,23 +1,20 @@
 import React, {useState} from 'react'
-import { DatePicker, message, Alert, Layout, Menu, Breadcrumb, Button, Row, Col} from 'antd';
+import { DatePicker, message, Alert, Layout, Menu, Breadcrumb, Button, Row, Col, Switch} from 'antd';
 import 'antd/dist/antd.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import LiveClock from './LiveClock';
+import Home from './Home';
+import OutWork from './OutWork';
 
 const { Header, Content, Sider, Footer } = Layout;
 
 function MainPage() {
-    //state
-    const [date, setDate] = useState('');
-    //state 값을 조건에 따라 변경하는 함수
-    const handleChange = value => {
-        message.info(`Selected Date: ${value ? value.format('YYYY-MM-DD') : 'None'}`);
-        setDate(value);
-    };
     //main
   return (
     <div>
+      <Router>
       <Layout style={{ minHeight: '100vh' }}>
-        <Sider>
+        <Sider style={{background:'dark'}}>
         <div>
         <LiveClock></LiveClock>
         </div>
@@ -29,9 +26,11 @@ function MainPage() {
           <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
             <Menu.Item key="1">
               <span>홈 바로가기</span>
+              <Link to="/" />
             </Menu.Item>
             <Menu.Item key="2">
               <span>연가</span>
+              <Link to="/outWork" />
             </Menu.Item>
             <Menu.Item key="3">
               <span>근무조회</span>
@@ -45,26 +44,20 @@ function MainPage() {
           </Menu>
         </Sider>
         <Layout>
-          <Header style={{ background: '#fff', padding: 0 }} />
+          <Header style={{ background: '#fff', padding: 0, textAlign: 'end' }} >
+            <Button style={{marginRight:'1%'}}>로그아웃</Button>
+          </Header>
           <Content style={{ margin: '0 16px' }}>
-            <Breadcrumb style={{ margin: '16px 0' }}>
-              <Breadcrumb.Item>
-              {/* 달력 */}
-                <DatePicker onChange={handleChange} />
-                <div style={{ marginTop: 16 }}>
-                    <Alert message="Selected Date" description={date ? date.format('YYYY-MM-DD') : 'None'} />
-                </div>
-              </Breadcrumb.Item>
-            </Breadcrumb>
-            <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
-              MainPage
-            </div>
+            <Route exact path="/" component={Home} />
+            {/* 바꾸고 새로고침 시 안에 내용이 사라짐. 수정해야함 */}
+            <Route exact path="/outWork" component={OutWork} />
           </Content>
           <Footer style={{ textAlign: 'center' }}>
             Ant Design ©2018 Created by Ant UED
           </Footer>
         </Layout>
       </Layout>
+      </Router>
     </div>
   );
 };
